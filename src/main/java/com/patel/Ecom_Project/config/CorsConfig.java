b70fe075a4e44a4fada21761b5585719917f8305
bhavnaPatel-6 <pbhavna0987@gmail.com>
2026-06-21 19:06:46 +0530

    fix CORS for Vercel frontend

--- src/main/java/com/patel/Ecom_Project/config/CorsConfig.java
+++ src/main/java/com/patel/Ecom_Project/config/CorsConfig.java
@@ -6,23 +6,27 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
+
     @Bean
     public CorsFilter corsFilter() {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.addAllowedOrigin("http://127.0.0.1:5500");
+        // ✅ FRONTEND URL (Vercel)
+        config.setAllowedOrigins(List.of(
+                "http://127.0.0.1:5500",
+                "https://ecommerce-product-hub.vercel.app"));
 
         config.addAllowedHeader("*");
-
         config.addAllowedMethod("*");
 
         config.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
         source.registerCorsConfiguration("/**", config);
 
