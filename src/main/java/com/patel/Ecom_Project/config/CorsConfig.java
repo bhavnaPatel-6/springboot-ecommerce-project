5181d478cc5771c77c62cdd7870f3db6d63d94fc
bhavnaPatel-6 <pbhavna0987@gmail.com>
2026-06-22 08:58:07 +0530

    fix CorsConfig imports

--- src/main/java/com/patel/Ecom_Project/config/CorsConfig.java
+++ src/main/java/com/patel/Ecom_Project/config/CorsConfig.java
@@ -1,3 +1,13 @@
+package com.patel.Ecom_Project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
 
